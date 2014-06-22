e92a66aad6e1b2685ae448e1626970d48437920e
Chris Northcott <cmn@cmn.me.uk>
2014-06-22 12:49:07 +0100

    updated makefile and example

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
 PROJ=test
 
-CFLAGS=-Wall -Wextra -Wimplicit-function-declaration -Wredundant-decls -Wstrict-prototypes -Wundef -Wshadow -g -fno-common -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -MD
-LDFLAGS=--static -lc -lnosys -T stm32.ld -nostartfiles -Wl,--gc-sections -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -lm -Wl,-Map=$(PROJ).map
+CFLAGS=-DSTM32F4 -I./ocm-includes -Wall -Wextra -Wimplicit-function-declaration -Wredundant-decls -Wstrict-prototypes -Wundef -Wshadow -g -fno-common -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -MD
+LDFLAGS=--static -L./ocm-libs -lc -lnosys -T stm32.ld -nostartfiles -Wl,--gc-sections -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -lm -Wl,-Map=$(PROJ).map
 
 CC=arm-none-eabi-gcc
 OBJCOPY=arm-none-eabi-objcopy
@@ -17,5 +17,11 @@ $(PROJ).bin: $(PROJ).elf
 	$(OBJCOPY) -Obinary $(PROJ).elf $(PROJ).bin
 
 $(PROJ).elf: $(OBJS)
-	$(CC) -o test.elf $(OBJS) $(TGTTYPE).a $(LDFLAGS)
+	$(CC) -o $(PROJ).elf $(OBJS) $(TGTTYPE).a $(LDFLAGS)
 
+test: $(PROJ).elf
+	echo "ST-Link should be running";
+	arm-none-eabi-gdb $(PROJ.elf) -ex "target extended-remote :4242" -ex "load" -ex "kill" -ex "continue"
+
+clean:
+	rm *.o *.elf *.bin *.map *.d
--- test.c
+++ test.c
@@ -1,23 +1,59 @@
-extern void setup(void);
-extern void loop(void);
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
-void setup()
+static void setup(void)
 {
-
+	//set up leds (D12-D15)
+	rcc_periph_clock_enable(RCC_GPIOD);
+	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12|GPIO13|GPIO14|GPIO15);
+	//set up button (A0)
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
 }
 
-void loop()
+static void wait(int us)
 {
-	__asm("nop");
+	int i;
+	for(i=0; i<us; i++){
+		__asm__("nop");
+	}
 }
 
-/* Arduino-like Setup/Loop structure */
+volatile int button_pressed = 0;
+
+static void loop()
+{
+	if((GPIOA_IDR & (1 << 0)) != 0){
+		if(button_pressed == 0)
+			button_pressed = 1;
+		else
+			button_pressed = 0;
+
+	if(button_pressed == 0)
+		gpio_clear(GPIOD, GPIO14);
+		gpio_clear(GPIOD, GPIO15);
+		gpio_toggle(GPIOD, GPIO12);
+		wait(50000);
+		gpio_toggle(GPIOD, GPIO13);
+		wait(50000);
+	}else{
+		gpio_clear(GPIOD, GPIO12);
+		gpio_clear(GPIOD, GPIO13);
+		gpio_toggle(GPIOD, GPIO14);
+		wait(50000);
+		gpio_toggle(GPIOD, GPIO15);
+		wait(50000);
+	}
+}
 
 int main(void)
 {
 	setup();
-	while(1)
-	{
+
+	/* Blink the LED (PC8) on the board. */
+	while (1) {
 		loop();
 	}
+
+	return 0;
 }
